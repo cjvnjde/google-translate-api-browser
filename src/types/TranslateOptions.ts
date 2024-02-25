fbee0c45eeffa46a7cbae5d5ed01533cdb83dda1
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2024-02-25 18:06:40 +0200

    Add support for custom headers in API requests

--- src/types/TranslateOptions.ts
+++ src/types/TranslateOptions.ts
@@ -1,9 +1,14 @@
 import { LangKey } from "./LangKey";
 
+type Headers = {
+  [key: string]: string | string[];
+}
+
 export type TranslateOptions = {
   from: LangKey;
   to: LangKey;
   hl: LangKey;
   tld: string;
   rpcids: string;
+  headers?: Headers;
 };
