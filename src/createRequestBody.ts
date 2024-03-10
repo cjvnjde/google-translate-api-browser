81b88775364082656db6db2f50044143e7d4efbb
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2024-03-10 14:33:14 +0200

    escape special characters

--- src/createRequestBody.ts
+++ src/createRequestBody.ts
@@ -1,15 +1,19 @@
 import { defaultTranslateOptions } from "./defaultTranslateOptions";
 import { CreateRequestBodyOptions } from "./types/CreateRequestBodyOptions";
 
-function escapeQuote(string: string): string {
-  return string.replace(/["]/g, "\\\\\\$&");  // $& means the whole matched string
+function escapeSpecialSymbols(inputString: string): string {
+    const escapedString = inputString.replace(/[\\`*_{}[\]()#+\-.!"]/g, '\\\\\\$&');
+    const normalizedString = escapedString.replace(/\r\n|\r|\n/g, '\\\\n');
+
+    return normalizedString;
 }
 
 export function createRequestBody(
   text: string,
   { rpcids = defaultTranslateOptions.rpcids, from = defaultTranslateOptions.from, to = defaultTranslateOptions.to }: CreateRequestBodyOptions = {},
 ): string {
-  const encodedData = encodeURIComponent(`[[["${rpcids}","[[\\"${escapeQuote(text)}\\",\\"${from}\\",\\"${to}\\",1],[]]",null,"generic"]]]`);
+  const normalizedText = escapeSpecialSymbols(text.trim());
+  const encodedData = encodeURIComponent(`[[["${rpcids}","[[\\"${normalizedText}\\",\\"${from}\\",\\"${to}\\",1],[]]",null,"generic"]]]`);
 
   return `f.req=${encodedData}&`;
 }
